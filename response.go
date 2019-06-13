90df89a705a3fd867c27730f8e5474339e7ce7f4
mumushuiding <181457121@qq.com>
2019-06-13 14:04:19 +0800

    commit

--- response.go
+++ response.go
@@ -7,16 +7,16 @@ import (
 
 // Response 返回信息给前台
 func Response(writer http.ResponseWriter, data string, ok bool) {
-	fmt.Fprintf(writer, "{\"data\":\"%s\",\"ok\":%t}", data, ok)
+	fmt.Fprintf(writer, "{\"message\":\"%s\",\"ok\":%t}", data, ok)
 	// return err
 }
 
 // ResponseErr 返回错误给前台
 func ResponseErr(w http.ResponseWriter, data interface{}) {
-	fmt.Fprintf(w, "{\"data\":\"%s\",\"ok\":%t}", data, false)
+	fmt.Fprintf(w, "{\"message\":\"%s\",\"ok\":%t}", data, false)
 }
 
 // ResponseOk 返回成功
 func ResponseOk(w http.ResponseWriter) {
-	fmt.Fprintf(w, "{\"data\":\"%s\",\"ok\":%t}", "成功", true)
+	fmt.Fprintf(w, "{\"message\":\"%s\",\"ok\":%t}", "成功", true)
 }
