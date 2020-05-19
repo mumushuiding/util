f565e0fa07e1442bafc4fce868af7c668b006676
mumushuiding <181457121@qq.com>
2020-05-19 10:45:49 +0800

    commit

--- regex.go
+++ regex.go
@@ -6,13 +6,15 @@ import (
 )
 
 // IsMobile 是否是电话号码
-func IsMobile(value string) (bool, error) {
-	return regexp.MatchString(`^\d{11}$`, value)
+func IsMobile(value string) bool {
+	yes, _ := regexp.MatchString(`^\d{11}$`, value)
+	return yes
 }
 
 // IsEmail 是否是邮箱
-func IsEmail(value string) (bool, error) {
-	return regexp.MatchString(`\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*`, value)
+func IsEmail(value string) bool {
+	yes, _ := regexp.MatchString(`\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*`, value)
+	return yes
 }
 
 // IsChinese 是否是中文
@@ -26,6 +28,7 @@ func IsChinese(value string) bool {
 }
 
 // IsABC 是否是英文字母
-func IsABC(value string) (bool, error) {
-	return regexp.MatchString(`[\w]$`, value)
+func IsABC(value string) bool {
+	yes, _ := regexp.MatchString(`[\w]$`, value)
+	return yes
 }
